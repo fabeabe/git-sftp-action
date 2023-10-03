121d8ad698231b3f4d5903ecf5210844c2a9a531
fabeabe <37873069+fabeabe@users.noreply.github.com>
2023-10-03 13:56:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 git ftp init # upload all once
+  --syncroot $INPUT_SYNCROOT \
+  --user $INPUT_USER \
+  --passwd $INPUT_PASSWORD \
+  --remote-root $INPUT_REMOTEROOT \
+  $INPUT_URL
 git-ftp push \
   --syncroot $INPUT_SYNCROOT \
   --user $INPUT_USER \
